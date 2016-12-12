b356a47785cf410b220a2935f152ebdc83478bb4
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-12-12 17:27:28 -0200

    Add text format modifiers for html5

--- lib/txt2tags.rb
+++ lib/txt2tags.rb
@@ -1,6 +1,26 @@
 require 'stringio'
 
 class Txt2Tags
+  @@marks = {
+    :monospace => '``(.*?)``',
+    :bold => '\*\*(.*?)\*\*',
+    :italic => '//(.*?)//',
+    :underline => '__(.*?)__',
+    :strike => '--(.*?)--',
+  }
+
+  @@targets = {
+    :html5 => {
+      :monospace => '<pre>\1</pre>',
+      :bold => '<strong>\1</strong>',
+      :italic => '<em>\1</em>',
+      :underline => '<span style="text-decoration: underline;">\1</span>',
+      :strike => '<span style="text-decoration: line-through;">\1</span>',
+    }
+  }
+
+  @@compiled = false
+
   def initialize(input)
     if input.respond_to?(:read)
       @input = input
@@ -9,13 +29,31 @@ class Txt2Tags
     else
       raise 'Cannot read this.'
     end
+
+    if not @@compiled
+      @@marks.keys.each do |m|
+        @@marks[m] = Regexp.new @@marks[m]
+      end
+
+      @@compiled = true
+    end
   end
 
-  def html
-    self.sanitize.join
+  def html5
+    self.apply_rules(:html5, self.sanitize).join
   end
 
-  def sanitize()
+  def sanitize
     @input.readlines.select { |l| not l.start_with?('%') }
+  end
+
+  def apply_rules(format, lines)
+    lines.each do |line|
+      @@marks.keys.each do |m|
+        line.gsub!(@@marks[m], @@targets[format][m])
+      end
+
+      yield line if block_given?
+    end
   end
 end
--- test/test_txt2tags.rb
+++ test/test_txt2tags.rb
@@ -4,11 +4,11 @@ require 'stringio'
 
 class TestInputParameter < Test::Unit::TestCase
   def test_string
-    assert_equal("string input", Txt2Tags.new("string input").html)
+    assert_equal("string input", Txt2Tags.new("string input").html5)
   end
 
   def test_file
-    assert_equal("file input", Txt2Tags.new(StringIO.new("file input")).html)
+    assert_equal("file input", Txt2Tags.new(StringIO.new("file input")).html5)
   end
 end
 
@@ -16,14 +16,34 @@ class TestRuleComments < Test::Unit::TestCase
   def test_remove_comment
     assert_equal(
       "Without comment\nNo comments...",
-      Txt2Tags.new("Without comment\n%Comment!\nNo comments...").html
+      Txt2Tags.new("Without comment\n%Comment!\nNo comments...").html5
     )
   end
 
   def test_mantain_line_with_percente_simbol_not_in_line_begin
     assert_equal(
       "Without comment\n %This isnt a Comment!\nNo comments...",
-      Txt2Tags.new("Without comment\n %This isnt a Comment!\nNo comments...").html
+      Txt2Tags.new("Without comment\n %This isnt a Comment!\nNo comments...").html5
     )
+  end
+
+  def test_html_output_monospace_rule
+    assert_equal("<pre>monospace</pre>", Txt2Tags.new("``monospace``").html5)
+  end
+
+  def test_html_output_bold_rule
+    assert_equal("<strong>bold</strong>", Txt2Tags.new("**bold**").html5)
+  end
+
+  def test_html_output_italic_rule
+    assert_equal("<em>italic</em>", Txt2Tags.new("//italic//").html5)
+  end
+
+  def test_html_output_underline_rule
+    assert_equal("<span style=\"text-decoration: underline;\">underline</span>", Txt2Tags.new("__underline__").html5)
+  end
+
+  def test_html_output_strike_rule
+    assert_equal("<span style=\"text-decoration: line-through;\">strike</span>", Txt2Tags.new("--strike--").html5)
   end
 end
