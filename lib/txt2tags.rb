14be8ab121ad225407946e4763bb78a9ab232f9f
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-12-13 13:52:14 -0200

    Fix style conforms rubocop

--- lib/txt2tags.rb
+++ lib/txt2tags.rb
@@ -3,55 +3,55 @@ require 'stringio'
 class Txt2Tags
   @@marks = {
     # Beautifiers
-    :monospace => {:type => :inline, :regexp => '``(.*?)``'},
-    :bold => {:type => :inline, :regexp => '\*\*(.*?)\*\*'},
-    :italic => {:type => :inline, :regexp => '//(.*?)//'},
-    :underline => {:type => :inline, :regexp => '__(.*?)__'},
-    :strike => {:type => :inline, :regexp => '--(.*?)--'},
+    monospace: { type: :inline, regexp: '``(.*?)``' },
+    bold: { type: :inline, regexp: '\*\*(.*?)\*\*' },
+    italic: { type: :inline, regexp: '//(.*?)//' },
+    underline: { type: :inline, regexp: '__(.*?)__' },
+    strike: { type: :inline, regexp: '--(.*?)--' },
 
     # Titles
-    :title1 => {:type => :title, :regexp => '\A= (.*?) ='},
-    :title2 => {:type => :title, :regexp => '\A== (.*?) =='},
-    :title3 => {:type => :title, :regexp => '\A=== (.*?) ==='},
+    title1: { type: :title, regexp: '\A= (.*?) =' },
+    title2: { type: :title, regexp: '\A== (.*?) ==' },
+    title3: { type: :title, regexp: '\A=== (.*?) ===' },
 
     # Blocks
-    :quote => {
-      :type => :block,
-      :regexp => {
-        :begin => '\A[ \t]+',
-        :end => '\A\s*\Z'
+    quote: {
+      type: :block,
+      regexp: {
+        begin: '\A[ \t]+',
+        end: '\A\s*\Z'
       },
-      :apply_inline => true,
-      :ignore_match_line => false,
+      apply_inline: true,
+      ignore_match_line: false
     },
-    :verbatim => {
-      :type => :block,
-      :regexp => {
-        :begin => '\A```\Z',
-        :end => '\A```\Z',
+    verbatim: {
+      type: :block,
+      regexp: {
+        begin: '\A```\Z',
+        end: '\A```\Z'
       },
-      :apply_inline => true,
-      :ignore_match_line => true,
+      apply_inline: true,
+      ignore_match_line: true
     }
   }
 
   @@targets = {
-    :html5 => {
+    html5: {
       # Beautifiers
-      :monospace => '<pre>\1</pre>',
-      :bold => '<strong>\1</strong>',
-      :italic => '<em>\1</em>',
-      :underline => '<span style="text-decoration: underline;">\1</span>',
-      :strike => '<span style="text-decoration: line-through;">\1</span>',
+      monospace: '<pre>\1</pre>',
+      bold: '<strong>\1</strong>',
+      italic: '<em>\1</em>',
+      underline: '<span style="text-decoration: underline;">\1</span>',
+      strike: '<span style="text-decoration: line-through;">\1</span>',
 
       # Titles
-      :title1 => '<h2>\1</h2>',
-      :title2 => '<h3>\1</h3>',
-      :title3 => '<h4>\1</h4>',
+      title1: '<h2>\1</h2>',
+      title2: '<h3>\1</h3>',
+      title3: '<h4>\1</h4>',
 
       # Blocks
-      :quote => {:begin => "<blockquote>\n", :end => "</blockquote>\n"},
-      :verbatim => {:begin => "<pre>\n", :end => "</pre>\n"},
+      quote: { begin: "<blockquote>\n", end: "</blockquote>\n" },
+      verbatim: { begin: "<pre>\n", end: "</pre>\n" }
     }
   }
 
@@ -74,27 +74,14 @@ class Txt2Tags
       raise 'Cannot read this.'
     end
 
-    if not @@compiled
-      @@marks.keys.each do |m|
-        case @@marks[m][:type]
-        when :inline, :title
-          @@marks[m][:regexp] = Regexp.new @@marks[m][:regexp]
-        when :block
-          [:begin, :end].each do |t|
-            @@marks[m][:regexp][t] = Regexp.new @@marks[m][:regexp][t]
-          end
-        else
-          raise "Mark type [#{@@marks[m][:type]}] not recognized."
-        end
-      end
+    return if @@compiled
 
-      @@compiled = true
+    compile_regexp
+    @@compiled = true
 
-      @@compiled.freeze
-      @@marks.freeze
-      @@targets.freeze
-
-    end
+    @@compiled.freeze
+    @@marks.freeze
+    @@targets.freeze
   end
 
   def html5
@@ -102,6 +89,21 @@ class Txt2Tags
   end
 
   private
+
+  def compile_regexp
+    @@marks.keys.each do |m|
+      case @@marks[m][:type]
+      when :inline, :title
+        @@marks[m][:regexp] = Regexp.new @@marks[m][:regexp]
+      when :block
+        [:begin, :end].each do |t|
+          @@marks[m][:regexp][t] = Regexp.new @@marks[m][:regexp][t]
+        end
+      else
+        raise "Mark type [#{@@marks[m][:type]}] not recognized."
+      end
+    end
+  end
 
   def apply_rules(format)
     in_block = false
@@ -113,7 +115,7 @@ class Txt2Tags
     close_ignore_current_line = false
 
     Enumerator.new do |y|
-      @input.readlines.reject{|l| l.start_with?('%')}.each do |line|
+      @input.readlines.reject { |l| l.start_with?('%') }.each do |line|
         if in_block
           if line.match(close_block_re)
             in_block = false
@@ -123,7 +125,7 @@ class Txt2Tags
             y.yield close_block
           end
         else
-          @@marks.keys.select{|k| @@marks[k][:type]==:block}.each do |m|
+          @@marks.keys.select { |k| @@marks[k][:type] == :block }.each do |m|
             if line.match(@@marks[m][:regexp][:begin])
               in_block = true
               ignore_current_line = @@marks[m][:ignore_match_line]
@@ -137,13 +139,10 @@ class Txt2Tags
           end
         end
 
-        if not ignore_current_line
+        if !ignore_current_line
           @@marks
             .keys
-            .select{|k|
-               (apply_inline && @@marks[k][:type]==:inline) or
-               (apply_title && @@marks[k][:type]==:title)
-            }
+            .select { |k| (apply_inline && @@marks[k][:type] == :inline) || (apply_title && @@marks[k][:type] == :title) }
             .each do |m|
             line.gsub!(@@marks[m][:regexp], @@targets[format][m])
           end
