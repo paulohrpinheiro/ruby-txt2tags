8bda6c34f0e4d4a1fb3bb9f5461d2877da64bb2d
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-12-13 16:19:13 -0200

    Remove class var and compile regexp in attribution

--- lib/txt2tags.rb
+++ lib/txt2tags.rb
@@ -1,69 +1,8 @@
 require 'stringio'
 
+# TXT2Tags - Converts t2t files to another formats
 class Txt2Tags
-  @@marks = {
-    # Beautifiers
-    monospace: { type: :inline, regexp: '``(.*?)``' },
-    bold: { type: :inline, regexp: '\*\*(.*?)\*\*' },
-    italic: { type: :inline, regexp: '//(.*?)//' },
-    underline: { type: :inline, regexp: '__(.*?)__' },
-    strike: { type: :inline, regexp: '--(.*?)--' },
-
-    # Titles
-    title1: { type: :title, regexp: '\A= (.*?) =' },
-    title2: { type: :title, regexp: '\A== (.*?) ==' },
-    title3: { type: :title, regexp: '\A=== (.*?) ===' },
-
-    # Blocks
-    quote: {
-      type: :block,
-      regexp: {
-        begin: '\A[ \t]+',
-        end: '\A\s*\Z'
-      },
-      apply_inline: true,
-      ignore_match_line: false
-    },
-    verbatim: {
-      type: :block,
-      regexp: {
-        begin: '\A```\Z',
-        end: '\A```\Z'
-      },
-      apply_inline: true,
-      ignore_match_line: true
-    }
-  }
-
-  @@targets = {
-    html5: {
-      # Beautifiers
-      monospace: '<pre>\1</pre>',
-      bold: '<strong>\1</strong>',
-      italic: '<em>\1</em>',
-      underline: '<span style="text-decoration: underline;">\1</span>',
-      strike: '<span style="text-decoration: line-through;">\1</span>',
-
-      # Titles
-      title1: '<h2>\1</h2>',
-      title2: '<h3>\1</h3>',
-      title3: '<h4>\1</h4>',
-
-      # Blocks
-      quote: { begin: "<blockquote>\n", end: "</blockquote>\n" },
-      verbatim: { begin: "<pre>\n", end: "</pre>\n" }
-    }
-  }
-
-  @@compiled = false
-
-  def self.marks_content
-    @@marks
-  end
-
-  def self.targets_content
-    @@targets
-  end
+  attr_reader :marks, :targets
 
   def initialize(input)
     if input.respond_to?(:read)
@@ -74,14 +13,59 @@ class Txt2Tags
       raise 'Cannot read this.'
     end
 
-    return if @@compiled
+    @targets = {
+      html5: {
+        # Beautifiers
+        monospace: '<pre>\1</pre>',
+        bold: '<strong>\1</strong>',
+        italic: '<em>\1</em>',
+        underline: '<span style="text-decoration: underline;">\1</span>',
+        strike: '<span style="text-decoration: line-through;">\1</span>',
 
-    compile_regexp!
-    @@compiled = true
+        # Titles
+        title1: '<h2>\1</h2>',
+        title2: '<h3>\1</h3>',
+        title3: '<h4>\1</h4>',
 
-    @@compiled.freeze
-    @@marks.freeze
-    @@targets.freeze
+        # Blocks
+        quote: { begin: "<blockquote>\n", end: "</blockquote>\n" },
+        verbatim: { begin: "<pre>\n", end: "</pre>\n" }
+      }
+    }
+
+    @marks = {
+      # Beautifiers
+      monospace: { type: :inline, regexp: Regexp.new('``(.*?)``') },
+      bold: { type: :inline, regexp: Regexp.new('\*\*(.*?)\*\*') },
+      italic: { type: :inline, regexp: Regexp.new('//(.*?)//') },
+      underline: { type: :inline, regexp: Regexp.new('__(.*?)__') },
+      strike: { type: :inline, regexp: Regexp.new('--(.*?)--') },
+
+      # Titles
+      title1: { type: :title, regexp: Regexp.new('\A= (.*?) =') },
+      title2: { type: :title, regexp: Regexp.new('\A== (.*?) ==') },
+      title3: { type: :title, regexp: Regexp.new('\A=== (.*?) ===') },
+
+      # Blocks
+      quote: {
+        type: :block,
+        regexp: {
+          begin: Regexp.new('\A[ \t]+'),
+          end: Regexp.new('\A\s*\Z')
+        },
+        apply_inline: true,
+        ignore_match_line: false
+      },
+      verbatim: {
+        type: :block,
+        regexp: {
+          begin: Regexp.new('\A```\Z'),
+          end: Regexp.new('\A```\Z')
+        },
+        apply_inline: true,
+        ignore_match_line: true
+      }
+    }
   end
 
   def html5
@@ -89,18 +73,6 @@ class Txt2Tags
   end
 
   private
-
-  def compile_regexp!
-    @@marks.keys.each do |m|
-      if [:inline, :title].include?(@@marks[m][:type])
-        @@marks[m][:regexp] = Regexp.new @@marks[m][:regexp]
-      else
-        [:begin, :end].each do |t|
-          @@marks[m][:regexp][t] = Regexp.new @@marks[m][:regexp][t]
-        end
-      end
-    end
-  end
 
   def apply_rules(format)
     in_block = false
@@ -122,17 +94,17 @@ class Txt2Tags
             y.yield close_block
           end
         else
-          @@marks.keys.select { |k| @@marks[k][:type] == :block }.each do |m|
-            next unless line.match(@@marks[m][:regexp][:begin])
+          @marks.keys.select { |k| @marks[k][:type] == :block }.each do |m|
+            next unless line.match(@marks[m][:regexp][:begin])
 
             in_block = true
-            ignore_current_line = @@marks[m][:ignore_match_line]
+            ignore_current_line = @marks[m][:ignore_match_line]
             close_ignore_current_line = ignore_current_line
-            apply_inline = @@marks[m][:apply_inline]
-            close_block = @@targets[format][m][:end]
-            close_block_re = @@marks[m][:regexp][:end]
+            apply_inline = @marks[m][:apply_inline]
+            close_block = @targets[format][m][:end]
+            close_block_re = @marks[m][:regexp][:end]
 
-            y.yield @@targets[format][m][:begin]
+            y.yield @targets[format][m][:begin]
           end
         end
 
@@ -151,8 +123,8 @@ class Txt2Tags
   end
 
   def apply_single_marks!(mark, format, line)
-    @@marks.keys.select { |k| @@marks[k][:type] == mark }.each do |m|
-      line.gsub!(@@marks[m][:regexp], @@targets[format][m])
+    @marks.keys.select { |k| @marks[k][:type] == mark }.each do |m|
+      line.gsub!(@marks[m][:regexp], @targets[format][m])
     end
   end
 end
