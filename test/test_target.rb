2c494e6f04d88e1f7e2e754bade2e8f0d43bdd13
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-12-13 16:18:23 -0200

    Use instance variable

--- test/test_target.rb
+++ test/test_target.rb
@@ -2,9 +2,10 @@ require_relative 'helper_test'
 
 class TestTarget < Test::Unit::TestCase
   def test_has_all_marks
-    all_marks = Txt2Tags::marks_content.keys.sort
-    Txt2Tags::targets_content.keys.each do |t|
-      assert_equal(all_marks, Txt2Tags::targets_content[t].keys.sort,
+    txt2tags = Txt2Tags.new ''
+    all_marks = txt2tags.marks.keys.sort
+    txt2tags.targets.keys.each do |t|
+      assert_equal(all_marks, txt2tags.targets[t].keys.sort,
        "#{t} does not contain all the marks.")
     end
   end
