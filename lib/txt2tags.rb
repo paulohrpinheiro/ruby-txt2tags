ef5373fd86edae4b29fa08fde6a0afd63fd0777d
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-12-13 04:52:54 -0200

    Change struct for marks and for targets

--- lib/txt2tags.rb
+++ lib/txt2tags.rb
@@ -3,16 +3,36 @@ require 'stringio'
 class Txt2Tags
   @@marks = {
     # Beautifiers
-    :monospace => '``(.*?)``',
-    :bold => '\*\*(.*?)\*\*',
-    :italic => '//(.*?)//',
-    :underline => '__(.*?)__',
-    :strike => '--(.*?)--',
+    :monospace => {:type => :inline, :regexp => '``(.*?)``'},
+    :bold => {:type => :inline, :regexp => '\*\*(.*?)\*\*'},
+    :italic => {:type => :inline, :regexp => '//(.*?)//'},
+    :underline => {:type => :inline, :regexp => '__(.*?)__'},
+    :strike => {:type => :inline, :regexp => '--(.*?)--'},
 
     # Titles
-    :title1 => '\A= (.*?) =',
-    :title2 => '\A== (.*?) ==',
-    :title3 => '\A=== (.*?) ===',
+    :title1 => {:type => :title, :regexp => '\A= (.*?) ='},
+    :title2 => {:type => :title, :regexp => '\A== (.*?) =='},
+    :title3 => {:type => :title, :regexp => '\A=== (.*?) ==='},
+
+    # Blocks
+    :quote => {
+      :type => :block,
+      :regexp => {
+        :begin => '\A[ \t]+',
+        :end => '\A\s*\Z'
+      },
+      :apply_inline => true,
+      :ignore_match_line => false,
+    },
+    :verbatim => {
+      :type => :block,
+      :regexp => {
+        :begin => '\A```\Z',
+        :end => '\A```\Z',
+      },
+      :apply_inline => true,
+      :ignore_match_line => true,
+    }
   }
 
   @@targets = {
@@ -28,6 +48,10 @@ class Txt2Tags
       :title1 => '<h2>\1</h2>',
       :title2 => '<h3>\1</h3>',
       :title3 => '<h4>\1</h4>',
+
+      # Blocks
+      :quote => {:begin => "<blockquote>\n", :end => "</blockquote>\n"},
+      :verbatim => {:begin => "<pre>\n", :end => "</pre>\n"},
     }
   }
 
@@ -52,7 +76,16 @@ class Txt2Tags
 
     if not @@compiled
       @@marks.keys.each do |m|
-        @@marks[m] = Regexp.new @@marks[m]
+        case @@marks[m][:type]
+        when :inline, :title
+          @@marks[m][:regexp] = Regexp.new @@marks[m][:regexp]
+        when :block
+          [:begin, :end].each do |t|
+            @@marks[m][:regexp][t] = Regexp.new @@marks[m][:regexp][t]
+          end
+        else
+          raise "Mark type [#{@@marks[m][:type]}] not recognized."
+        end
       end
 
       @@compiled = true
@@ -65,22 +98,63 @@ class Txt2Tags
   end
 
   def html5
-    apply_rules(:html5, sanitize).join
+    apply_rules(:html5).to_a.join
   end
 
   private
 
-  def sanitize
-    @input.readlines.select { |l| not l.start_with?('%') }
-  end
+  def apply_rules(format)
+    in_block = false
+    apply_inline = true
+    apply_title = true
+    close_block = nil
+    close_block_re = nil
+    ignore_current_line = false
+    close_ignore_current_line = false
 
-  def apply_rules(format, lines)
-    lines.each do |line|
-      @@marks.keys.each do |m|
-        line.gsub!(@@marks[m], @@targets[format][m])
+    Enumerator.new do |y|
+      @input.readlines.reject{|l| l.start_with?('%')}.each do |line|
+        if in_block
+          if line.match(close_block_re)
+            in_block = false
+            apply_inline = true
+
+            ignore_current_line = close_ignore_current_line
+            y.yield close_block
+          end
+        else
+          @@marks.keys.select{|k| @@marks[k][:type]==:block}.each do |m|
+            if line.match(@@marks[m][:regexp][:begin])
+              in_block = true
+              ignore_current_line = @@marks[m][:ignore_match_line]
+              close_ignore_current_line = ignore_current_line
+              apply_inline = @@marks[m][:apply_inline]
+              close_block = @@targets[format][m][:end]
+              close_block_re = @@marks[m][:regexp][:end]
+
+              y.yield @@targets[format][m][:begin]
+            end
+          end
+        end
+
+        if not ignore_current_line
+          @@marks
+            .keys
+            .select{|k|
+               (apply_inline && @@marks[k][:type]==:inline) or
+               (apply_title && @@marks[k][:type]==:title)
+            }
+            .each do |m|
+            line.gsub!(@@marks[m][:regexp], @@targets[format][m])
+          end
+
+          y.yield line
+        else
+          ignore_current_line = false
+        end
       end
 
-      yield line if block_given?
+      y.yield close_block if in_block
     end
   end
 end
